cd5ce2bcac0ec53f34f2b5951c4415b859ad7470
Andre Faught <afaught@gmail.com>
2015-06-22 09:23:52 +0200

    Added application layer for fault repair

---
+++ RoadMaintenance.FaultRepair.Application/WorkOrderHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using RoadMaintenance.FaultRepair.Services;
+using RoadMaintenance.FaultRepair.Core;
+using RoadMaintenance.FaultRepair.Repos;
+using Ninject;
+
+namespace RoadMaintenance.FaultRepair.Application
+{
+    // Class representing a work order handler in the application layer
+
+    public class WorkOrderHandler
+    {
+        public WorkOrderHandler()
+        {
+        }
+
+        public string CreateWorkOrder(string       description, 
+                                      List<string> tasks,
+                                      List<Tuple<string, int>> equipment,
+                                      List<Tuple<string, double, MeasurementType>> materials)
+        {
+            // Use Ninject to inject the work order repository
+
+            IKernel kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            IWorkOrderRepository woRepo = kernel.Get<IWorkOrderRepository>();
+            WorkOrderService wos = new WorkOrderService(woRepo);
+
+            return wos.CreateWorkOrder(description, tasks, equipment, materials);
+        }
+    }
+}
