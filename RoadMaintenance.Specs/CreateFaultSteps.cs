8c5fcf7abd06967923b548f780e39b328e29cb62
Chris Jones <cjsunnysa@hotmail.com>
2015-06-30 08:03:18 +0200

    rename add fault to create fault.

---
+++ RoadMaintenance.Specs/CreateFaultSteps.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace RoadMaintenance.SharedKernel.Specs....RoadMaintenance.Specs
+{
+    [Binding]
+    public class AddNewFaultSteps
+    {
+        [Given(@"I am a '(.*)' user role")]
+        public void GivenIAmAUserRole(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter '(.*)' as the street name")]
+        public void GivenIEnterAsTheStreetName(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter '(.*)' as the cross street name")]
+        public void GivenIEnterAsTheCrossStreetName(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter '(.*)' as the suburb name")]
+        public void GivenIEnterAsTheSuburbName(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter '(.*)' as the description")]
+        public void GivenIEnterAsTheDescription(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I select '(.*)' as the fault priority")]
+        public void GivenISelectAsTheFaultPriority(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I select '(.*)' as the fault type")]
+        public void GivenISelectAsTheFaultType(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press the Create button")]
+        public void WhenIPressTheCreateButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I perform a find for this fault id")]
+        public void WhenIPerformAFindForThisFaultId()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"result should contain these details")]
+        public void ThenResultShouldContainTheseDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result has '(.*)' as the status")]
+        public void ThenTheResultHasAsTheStatus(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
